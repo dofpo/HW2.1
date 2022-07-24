aa9651e2fb00a2f165174cb79c3de4b6c640083d
dofpo <z-salawat91@mail.ru>
2022-07-24 09:09:10 +0500

    initial first commit

---
+++ src/test/java/ru/netology/test/restTest.java
@@ -0,0 +1,27 @@
+package ru.netology.test;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class restTest {
+    @Test
+    void Test() {
+        // Given - When - Then
+// Предусловия
+        // Given - When - Then
+// Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("Petr") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+                .when()
+                .post("/post")
+// Проверки
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("Name"))
+        ;
+    }
+}
